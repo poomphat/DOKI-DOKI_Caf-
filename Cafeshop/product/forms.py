04db6f57ecff49d960c2423cf8154e6b9f81a5fa
poomphat <42997298+poomphat@users.noreply.github.com>
2020-04-04 16:16:12 +0700

    add

--- Cafeshop/product/forms.py
+++ Cafeshop/product/forms.py
@@ -4,11 +4,17 @@ from product.models import Fruit , Option
 class FruitForm(forms.ModelForm):
     fruit_name = forms.CharField(
         max_length=30,
-        widget=forms.TextInput(attrs={ 'class' : 'form-control col-5' })
+        widget=forms.TextInput(attrs={ 'class' : 'form-control col-5',
+                                        'placeholder' : 'ชื่อของผลไม้' }),
+        
+        label='Fruit name:'
         )
     fruit_desc = forms.CharField(
         max_length=50,
-        widget=forms.TextInput(attrs={ 'class' : 'form-control col-12' })
+        widget=forms.TextInput(attrs={ 'class' : 'form-control col-12', 
+                                        'placeholder' : 'คำอธิบายของผลไม้'}),
+        
+        label='Fruit description:'
         )
     class Meta:
         model = Fruit
@@ -18,13 +24,16 @@ class FruitForm(forms.ModelForm):
 class OptionForm(forms.ModelForm):
     option_name = forms.CharField(
         max_length=30,
-        widget=forms.TextInput(attrs={ 'class' : 'form-control col-5' })
+        widget=forms.TextInput(attrs={ 'class' : 'form-control col-5', 'placeholder' : 'ชื่อของ topping' }),
+        label='Topping name:'
         )
     description = forms.CharField(
         max_length=255,
-        widget=forms.TextInput(attrs={ 'class' : 'form-control col-12' })
+        widget=forms.TextInput(attrs={ 'class' : 'form-control col-12' , 'placeholder' : 'คำอธิบายของ topping' }),
+        label='Topping description:'
         )
-    price = forms.CharField(max_length=10, widget=forms.TextInput(attrs={'type':'number', 'class' : 'form-control col-12'}))
+    price = forms.CharField(max_length=10, widget=forms.TextInput(attrs={'type':'number', 'class' : 'form-control col-12' ,'placeholder' : 'ราคาของ topping'}),
+        label='Topping price:')
     class Meta:
         model = Option
         fields = ('option_name', 'description','price')
